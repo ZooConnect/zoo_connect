e3514a6cbca6e54135a083794fe3787b2cc45c84
Harreshh <harreshha@gmail.com>
2025-12-04 14:58:07 +0100

    test(SCRUM-23): add integration tests for user update

--- src/app.js
+++ src/app.js
@@ -1,30 +1,23 @@
 import express from "express";
-
-// 1. Import your new User route
 import userRoutes from "./routes/user.routes.js";
-
-// 2. Import the existing skeleton routes (Do not remove these, they are needed for DevOps tests)
+// Import the "Skeleton" routes (Don't delete these!)
 import versionRoute from "./routes/auto/version.route.js";
 import infoRoute from "./routes/auto/info.route.js";
 import boomRoute from "./routes/auto/boom.route.js";
 
 const app = express();
 
-// 3. Middleware: This allows the app to understand JSON data sent in the request body
-// Without this, req.body will be undefined in your controller!
 app.use(express.json());
 
-// 4. Register the Routes
-app.use("/api/users", userRoutes); // This activates your new feature
+// Your Feature
+app.use("/api/users", userRoutes);
 
-// Register the skeleton routes (Keep these)
+// The Skeleton Features (Needed for CI to pass)
 app.use("/version", versionRoute);
 app.use("/info", infoRoute);
 app.use("/boom", boomRoute);
 
-// 5. Global Error Handler (Good practice)
 app.use((err, req, res, next) => {
-  console.error(err);
   res.status(500).json({ error: "Internal Server Error" });
 });
 
