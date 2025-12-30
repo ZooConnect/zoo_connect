331017705bb668740d4c026e9b9a490da769fb21
yalpha <clement.naissans@gmail.com>
2025-12-30 21:15:56 +0100

    suppression user.route.js et user.controller.js et ajout de auth.route.js et auth.controller.js

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,119 @@
+import * as userService from "../services/user.service.js";
+
+import { respond } from "../utils/response.helper.js";
+import { comparePassword, hashPassword, validatePassword } from "../utils/password.helper.js";
+import { createToken } from "../utils/jwt.helper.js";
+import { createCookie, clearCookie } from "../utils/cookie.helper.js";
+
+import MESSAGES from "../constants/messages.js";
+
+
+export const signup = async (req, res, next) => {
+    try {
+        const { name, email, password, passwordConfirmation } = req.body;
+
+        if (!name || !email || !password || !passwordConfirmation)
+            return respond(res, MESSAGES.AUTH.MISSING_FIELDS);
+        if (password !== passwordConfirmation)
+            return respond(res, MESSAGES.AUTH.PASSWORDS_DO_NOT_MATCH);
+
+        const passwordIsValidate = validatePassword(password);
+        if (!passwordIsValidate) return respond(res, MESSAGES.AUTH.PASSWORD_INVALID);
+
+        const isUserExisting = await userService.isUserExisting(email);
+        if (isUserExisting) return respond(res, MESSAGES.AUTH.EMAIL_ALREADY_USED);
+
+        const user = await userService.registerUser({ name, email, password });
+        respond(res, MESSAGES.AUTH.ACCOUNT_CREATED_SUCCESS, { user: { id: user._id, name: user.name, email: user.email } });
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) return respond(res, MESSAGES.AUTH.MISSING_FIELDS);
+
+        const user = await userService.findUserByEmail(email);
+        if (!user) return respond(res, MESSAGES.AUTH.INVALID_CREDENTIALS);
+
+        const valid = await comparePassword(password, user.passwordHash);
+        if (!valid) return respond(res, MESSAGES.AUTH.INVALID_CREDENTIALS);
+
+        const payload = {
+            id: user._id || user.id,
+            email: user.email,
+            name: user.name
+        };
+        const token = createToken(payload);
+        createCookie(res, token);
+
+        respond(res, MESSAGES.AUTH.LOGIN_SUCCESS, {
+            id: user._id,
+            name: user.name
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const logout = (req, res) => {
+    clearCookie(res);
+    respond(res, MESSAGES.AUTH.LOGOUT_SUCCESS);
+}
+
+export const getUser = (req, res) => {
+    // req.user est injecté par authMiddleware
+    respond(res, MESSAGES.USER.FOUND, {
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
+}
+
+export const updateUser = async (req, res, next) => {
+    try {
+        const userId = req.user.id;
+        const updates = req.body;
+
+        if (updates.email) {
+            const isUserExisting = await userService.isUserExisting(updates.email);
+            if (isUserExisting) return respond(res, MESSAGES.AUTH.EMAIL_ALREADY_USED);
+        }
+
+        if (updates.newPassword && updates.newPasswordConfirmation) {
+            if (updates.newPassword !== updates.newPasswordConfirmation)
+                return respond(res, MESSAGES.AUTH.PASSWORDS_DO_NOT_MATCH);
+
+            const passwordIsValidate = validatePassword(updates.newPassword);
+            if (!passwordIsValidate) respond(res, MESSAGES.AUTH.PASSWORD_INVALID);
+            updates.passwordHash = hashPassword(updates.newPassword);
+        }
+
+        const updatedUser = await userService.modifyUser(userId, updates);
+        if (!updatedUser) {
+            return respond(res, MESSAGES.USER.NOT_FOUND);
+        }
+
+        respond(res, MESSAGES.USER.VALID_MODIFICATION);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getMembership = async (req, res, next) => {
+    try {
+        const email = req.params.email;
+        const user = await userService.getUserInfo(email);
+        if (!user) return respond(res, MESSAGES.USER.NOT_FOUND);
+
+        respond(res, MESSAGES.USER.FOUND, {
+            membershipType: user.membershipType,
+            membershipExpirationDate: user.membershipExpirationDate,
+            memberShipStatus: user.memberShipStatus,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,18 +1,18 @@
-import jwt from 'jsonwebtoken';
+import * as userService from "../services/user.service.js";
 
-import User from "../models/user.model.js";
+import { verifyToken } from "../utils/jwt.helper.js";
+import { respond } from "../utils/response.helper.js";
 
 import MESSAGES from "../constants/messages.js";
 
 export default async (req, res, next) => {
   const token = req.cookies.token;
-  if (!token) return res.status(401).send(MESSAGES.AUTH.INVALID_CREDENTIALS);
+  if (!token) return respond(res, MESSAGES.AUTH.INVALID_CREDENTIALS);
 
   try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(payload.id).select("-passwordHash");
-    if (!user) return res.status(401).send();
-
+    const payload = verifyToken(token);
+    const user = await userService.findUserByEmail(payload.email);
+    if (!user) return respond(res, MESSAGES.USER.NOT_FOUND);
     req.user = user;
     next();
   } catch (error) {
