72a8c43caffd94985add604ba40371dea6b9bca4
yalpha <clement.naissans@gmail.com>
2026-01-03 15:49:19 +0100

    prends en compte les commits de léa mais qui a écrasé les commits de clément

--- backend/src/middlewares/admin/requireRole.middleware.js
+++ backend/src/middlewares/admin/requireRole.middleware.js
@@ -1,22 +1,6 @@
-import { CustomError } from "../errorHandler.js";
-
-const ROLE_HIERARCHY = {
-    admin: 3,
-    staff: 2,
-    visitor: 1
-};
+import * as usersService from "../../services/admin/users.service.js";
 
 export const requireRole = (minRole) => (req, res, next) => {
-    if (!req.user) {
-        throw new CustomError("Unauthorized", 401);
-    }
-
-    const userRoleLevel = ROLE_HIERARCHY[req.user.role] || 0;
-    const minRoleLevel = ROLE_HIERARCHY[minRole] || 0;
-
-    if (userRoleLevel < minRoleLevel) {
-        throw new CustomError("Access denied", 403);
-    }
-
+    usersService.requireRole(req.user, minRole);
     next();
 };
